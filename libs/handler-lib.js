03afca72f3dd041db6c837dc0c2f34e24db61128
rsharm18 <40524914+rsharm18@users.noreply.github.com>
2020-08-10 14:55:02 -0500

    adding handlerlib

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,16 +1,17 @@
 export default function handler(lambda) {
     return async function (event, context) {
-        let body, statusCode;
+        let body;
+        let statusCode;
 
-        //console.log(` event from handler  ${JSON.stringify(event)} \n\n `);
-        //console.log(` lambda - ${lambda}`);
+        // console.log(` event from handler  ${JSON.stringify(event)} \n\n `);
+        // console.log(` lambda - ${lambda}`);
 
         try {
             // Run the Lambda
             body = await lambda(event, context);
             statusCode = 200;
         } catch (e) {
-            //console.log(` \n error from handler ${JSON.stringify(e.stringify)}\n `);
+            // console.log(` \n error from handler ${JSON.stringify(e.stringify)}\n `);
             body = { error: e.message };
             statusCode = 500;
         }
@@ -21,8 +22,8 @@ export default function handler(lambda) {
             body: JSON.stringify(body),
             headers: {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true,
-            },
+                "Access-Control-Allow-Credentials": true
+            }
         };
     };
-}
+};
